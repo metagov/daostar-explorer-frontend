9bce929da96d9347266b2c75e425899e6eaedcb6
Fernando Mendes <fernando@mendes.codes>
2023-07-27 09:21:52 +0100

    Update allowlisted users

--- src/lib/wallet/reputable/users.ts
+++ src/lib/wallet/reputable/users.ts
@@ -7,6 +7,10 @@ export const Users = {
   "0xdf2cdaf893d04b6032e941669f77086e9caf5d86": 102,
   "0x7926dad04fe7c482425d784985b5e24aea03c9ff": 103,
   "0x3121a6f0c30b0ab4c713fa66d3f369ac12d364fd": 104,
+  "0x7ace0b7a0cfb2980aa25310af5c2602144d58db2": 105,
+  "0x1e8ee48d0621289297693fc98914da2efdce1477": 106,
+  "0x1d829bdbd534a70fdf27e959e790ef1d64e10ef8": 107,
+  "0x88e50e06efb2b748e2b9670d2a6668237167382b": 108,
 };
 
 export const ReverseUsers = Object.fromEntries(
